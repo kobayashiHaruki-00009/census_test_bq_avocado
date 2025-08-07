53c0dc8a0b301dba3267fe6e444849cd32651501
kobayashiHaruki-00009 <haruki-kobayashi@vorn.co.jp>
2025-08-07 15:22:46 +0900

    s

---
+++ models/dim/c.sql
@@ -0,0 +1,14 @@
+{{
+    {{
+      config(
+        materialized = 'table',
+        )
+    }}
+}}
+
+select `顧客キー`,
+    `顧客名`,
+    `アカウント残高`,
+    "教育" as `市場セグメント`,
+    "00" as `国家キー`
+from {{ ref('src_customer') }}
